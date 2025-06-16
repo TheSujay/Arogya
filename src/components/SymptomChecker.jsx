9292d91f972dc9f63530c57221cd126875ada6e7
unknown <sujaysahis73@gmail.com>
2025-06-16 09:15:55 +0530

    Connected to Prediction Server

--- src/components/SymptomChecker.jsx
+++ src/components/SymptomChecker.jsx
@@ -36,7 +36,7 @@ const SymptomChecker = () => {
     setSuggestions([]);
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+      const res = await fetch("https://arogya-ai.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ symptoms: symptomText }),
